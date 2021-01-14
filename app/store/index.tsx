3327ba49474c7d94627a9e3f980a21a77f200ad9
sdelvalle57 <sdelvalle57@gmail.com>
2021-01-14 14:51:58 -0500

    store index

--- app/store/index.tsx
+++ app/store/index.tsx
@@ -1,16 +1,25 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, AnyAction } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import ThunkMiddleware from 'redux-thunk'
+import thunk, { ThunkMiddleware } from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
+
 import rootReducer from '../reducers/index';
 import { Counter, StoreState } from '../types/store';
 
 /** If you want to add more than one middleware */
-const middlewares = [ThunkMiddleware, ReduxPromise]
 
 const counter: Counter = {
   count: 0
 }
+
+const extraArgument = {
+  
+};
+export type ExtraArgument = typeof extraArgument;
+
+const thunkMiddleware = thunk.withExtraArgument(extraArgument) as ThunkMiddleware<StoreState, AnyAction>;
+
+const middlewares = [thunkMiddleware, ReduxPromise]
 
 export function initializeStore (initialState: StoreState = {clock: null, counter}): any {
   return createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)))
